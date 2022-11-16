1ede3f5fcc95e1bdadc37cb8de6ab2d96f626269
Max <manishsalunkhe58@gmail.com>
2022-11-16 11:06:46 +0530

    Set

--- src/collectionframework/SetDemo.java
+++ src/collectionframework/SetDemo.java
@@ -1,9 +1,10 @@
 package collectionframework;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
-//set doesn't maintain order of
+//set doesn't maintain insertion order
 //set doesn't store duplicate values.
 public class SetDemo {
     public static void main(String[] args) {
@@ -20,6 +21,18 @@ public class SetDemo {
 
 
         Integer[] arr = set.toArray(new Integer[10]);
+
+        //to maintain the insertion order
+
+        Set<Integer> set1 = new LinkedHashSet<>();
+
+        set1.add(10);
+        set1.add(20);
+        set1.add(30);
+        set1.add(40);
+        set1.add(50);
+
+        System.out.println(set1);
 
     }
 }
