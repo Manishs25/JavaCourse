e476f983948a1a487923b18c6d323ecf61cc7910
Max <manishsalunkhe58@gmail.com>
2022-11-24 13:02:41 +0530

    StringBuffer and StringBuilder

--- src/stringdemo/StringBuilderDemo.java
+++ src/stringdemo/StringBuilderDemo.java
@@ -3,11 +3,11 @@ package stringdemo;
 public class StringBuilderDemo {
     public static void main(String[] args) {
 
-        StringBuilder stringBuilder = new StringBuilder("Pune");
+        StringBuilder stringBuilder = new StringBuilder("@Manish");
 
         System.out.println(stringBuilder.capacity());
         System.out.println(stringBuilder.length());
-        stringBuilder.append("satara");
+        stringBuilder.append("_X");
         System.out.println(stringBuilder);
         System.out.println(stringBuilder.indexOf("s"));
 //        System.out.println(stringBuilder.delete(0,4));
@@ -15,9 +15,8 @@ public class StringBuilderDemo {
         System.out.println(stringBuilder.lastIndexOf("r"));
         System.out.println(stringBuilder.isEmpty());
         System.out.println(stringBuilder.capacity());
-        System.out.println(stringBuilder.insert(4,'S'));
-        stringBuilder.setCharAt(4,'X');
+//        System.out.println(stringBuilder.insert(4,'S'));
+//        stringBuilder.setCharAt(4,'X');
         System.out.println(stringBuilder);
-        System.out.println(stringBuilder.replace(4,5,"XXX"));
     }
 }
