a77df0d6f247ab4fd7e9b7cae49c8a2cf55ce58f
Max <manishsalunkhe58@gmail.com>
2022-11-25 12:10:00 +0530

    Java8

---
+++ src/java8/DefaultAndStatic.java
@@ -0,0 +1,31 @@
+package java8;
+
+interface MyInterface2 {
+    static void test() {
+        System.out.println("In Test");
+    }
+
+    void show();
+
+    default void display() {
+        System.out.println("In Display");
+    }
+
+}
+
+public class DefaultAndStatic implements MyInterface2 {
+    public static void main(String[] args) {
+        DefaultAndStatic obj = new DefaultAndStatic();
+        obj.show();
+        obj.display();
+
+        MyInterface2.test();
+    }
+
+    @Override
+    public void show() {
+        System.out.println("In show");
+    }
+}
+
+
---
+++ src/java8/LambdaExpression.java
@@ -0,0 +1,42 @@
+package java8;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class LambdaExpression {
+    public static void main(String[] args) {
+
+        MyInterface m = () -> {
+            System.out.println("In lambda!!");
+        };
+        m.show();
+
+        MyInterface1 m1 = (i,j) -> {
+            System.out.println(i+j);
+        };
+        m1.add(2,2);
+
+
+        List<Integer> list = Arrays.asList(1,2,3,4);
+        list.forEach(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) {
+                System.out.println(integer);
+            }
+        });
+    }
+
+    @FunctionalInterface
+    interface MyInterface {
+        void show();
+    }
+    @FunctionalInterface
+    interface MyInterface1 {
+        void add(int i, int j);
+    }
+
+
+}
+
