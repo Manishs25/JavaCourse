9d5b6d859c0c0562ad96d6df78bfa2162422becb
Max <manishsalunkhe58@gmail.com>
2022-12-05 13:08:36 +0530

    Stream Api and Reduce Optional

--- src/java8/OptionalDemo.java
+++ src/java8/OptionalDemo.java
@@ -3,7 +3,7 @@ package java8;
 import java.util.Optional;
 
 public class OptionalDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         //optional is used to handle null pointer exception
         Student student = new Student(1, null, "Pune");
         if (student.name != null) {
@@ -17,12 +17,22 @@ public class OptionalDemo {
 //        Optional<String> s2 = Optional.of(student.name);
 //        System.out.println(s2);
 
-        Optional<String> s3 = Optional.ofNullable(student.name);
-        if (s3.isPresent()) {
-            System.out.println(s3.get());
-        } else {
-            System.out.println("offNullable");
-        }
+//        Optional<String> s3 = Optional.ofNullable(student.name);
+//        if (s3.isPresent()) {
+//            System.out.println(s3.get());
+//        }
+//        else {
+//            System.out.println("offNullable");
+//        }
+        Optional<String> s4 = Optional.ofNullable(student.name);
+        System.out.println(s4.orElseGet(() -> "Student is null"));
+
+        Optional<String> s5 = Optional.ofNullable(student.name);
+        System.out.println(s5.orElseThrow(() -> new Exception("exception")));
+
+        Optional<String> s6 = Optional.ofNullable(student.name);
+        System.out.println(s6.orElseThrow());
+
     }
 }
 
